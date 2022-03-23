7f435abbb79cf81bc5711dbdb9d1cf0a27d5bc22
David Sklar <davidhsklar@gmail.com>
2022-03-23 15:23:38 -0400

    finished the user repo feature

--- assets/JS/homepage.js
+++ assets/JS/homepage.js
@@ -8,12 +8,22 @@ var getUserRepos = function(user) {
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
   
     // make a request to the url
-    fetch(apiUrl).then(function(response) {
-        response.json().then(function(data) {
-            displayRepos(data, user);
-          });
-    });
-  };
+    fetch(apiUrl)
+  .then(function(response) {
+    // request was successful
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayRepos(data, user);
+      });
+    } else {
+      alert('Error: GitHub User Not Found');
+    }
+  })
+  .catch(function(error) {
+    // Notice this `.catch()` getting chained onto the end of the `.then()` method
+    alert("Unable to connect to GitHub");
+  });
+    };
   
 
   var formSubmitHandler = function(event) {
@@ -28,11 +38,18 @@ var getUserRepos = function(user) {
     }
 
     var displayRepos = function(repos, searchTerm) {
+       
+    // check if api returned any repos
+    if (repos.length === 0) {
+    repoContainer.textContent = "No repositories found.";
+    return;
+    }
         console.log(repos);
         console.log(searchTerm);
      // clear old content
     repoContainer.textContent = "";
     repoSearchTerm.textContent = searchTerm;
+
     // loop over repos
 for (var i = 0; i < repos.length; i++) {
     // format repo name
@@ -67,6 +84,6 @@ repoEl.appendChild(statusEl);
     // append container to the dom
     repoContainer.appendChild(repoEl);
   }
-      };
+      }
 
   userForm.addEventListener("submit", formSubmitHandler);
